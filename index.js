703bc5b3c342a74b37e55792c6a7cce0932d0abb
Edward Suwirya <edward.suwirya@btpn.com>
2017-06-06 20:06:56 +0700

    1st init

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by edo on 06/06/2017.
+ */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/Users/edo/NodeJSProjects/upload-mockup/upload')
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        cb(null, file.originalname);
+    }
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8888;
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.json({
+        responseCode: "0",
+        responseDescription: "[{\"articleGroupCode\":\"1\",\"articleGroupName\":\"2\"}," +
+        "{\"articleGroupCode\":\"3\",\"articleGroupName\":\"4\"}]"
+    });
+});
+
+router.post('/upload',  function (req, res) {
+    var upload = multer({
+        storage: storage
+    }).single('file');
+    upload(req, res, function (err) {
+        res.end('File is uploaded')
+    })
+})
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
